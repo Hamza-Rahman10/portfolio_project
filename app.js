3e0717f0565bbd4c4c9f48df14d1346710fada2f
Hamza-Rahman10 <hrahm002@campus.goldsmiths.ac.uk>
2024-12-08 22:04:43 +0000

    initial commit 8 signup in progress

--- app.js
+++ app.js
@@ -11,14 +11,23 @@ const app = express();
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set('view engine', 'ejs'); //set ejs like templates
+app.use(express.static('public')); //for static files like CSS
+
+// Use the signup route
+app.use('/signup', signupRoute);
+
+app.get('/signup', (req, res) => {
+  res.render('signup'); // Render the signup.ejs file
+});
+
+
 
 
 // Routes
 const mainRoutes = require('./routes/main'); // Home and about routes
 const searchRoutes = require('./routes/search'); // Search functionality
-const courseRoutes = require('./routes/courses'); // Add course functionality
+const courseRoutes = require('./routes/courses'); // course functionality
 
 app.use(mainRoutes);
 app.use(searchRoutes);
